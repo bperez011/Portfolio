b055f4bee3c4a56f3381b8871b5a64e27235ea89
bperez011 <112590275+bperez011@users.noreply.github.com>
2023-04-05 21:09:51 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -80,41 +80,49 @@ const Navbar = () => {
 
       {/* Social icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-        <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Linkedin <FaLinkedin size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Github <FaGithub size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Email <HiOutlineMail size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Resume <BsFillPersonLinesFill size={30} />
-            </a>
-          </li>
-        </ul>
-      </div>
+  <ul>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+      <a
+        className='flex justify-between items-center w-full text-gray-300'
+        href='https://www.linkedin.com/in/bictor-perez-okomura-092297230/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        LinkedIn <FaLinkedin size={30} />
+      </a>
+    </li>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+      <a
+        className='flex justify-between items-center w-full text-gray-300'
+        href='https://github.com/bperez011/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        GitHub <FaGithub size={30} />
+      </a>
+    </li>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+      <a
+        className='flex justify-between items-center w-full text-gray-300'
+        href='mailto:youremail@example.com'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Email <HiOutlineMail size={30} />
+      </a>
+    </li>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+      <a
+        className='flex justify-between items-center w-full text-gray-300'
+        href='https://yourwebsite.com/resume'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Resume <BsFillPersonLinesFill size={30} />
+      </a>
+    </li>
+  </ul>
+</div>
     </div>
   );
 };
